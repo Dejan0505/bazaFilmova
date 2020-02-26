0baedc3052d89d5fdc9dd63e545d40c67fca58d5
Damjan Pavlica (materijali) <damjan.pavlica@code.edu.rs>
2020-02-26 21:14:23 +0100

    dodata pretraga

--- js/pretraga.js
+++ js/pretraga.js
@@ -2,38 +2,35 @@ const wrapper = document.getElementById('wrapper');
 
 wrapper.innerHTML = "<img class='load' src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif'>";
 
+let filmovi = [];
 
 fetch('https://baza-filmova.herokuapp.com/filmovi/')
     .then(res => res.json())
-    .then(filmovi => {
-        console.log(filmovi);
-        wrapper.innerHTML= "";
-        for( i = 0;i<10;i++){
+    .then(data => {
+        filmovi = data;
+        wrapper.innerHTML = "";
+        for (i = 0; i < 10; i++) {
             wrapper.innerHTML += `
             <br> Naziv:  ${filmovi[i].naziv} <br>
             Godina: ${filmovi[i].godina} <br>
             Slika: <img src='${filmovi[i].slika}' height="200px", width="150px"> <hr>
-
             `;
-            
         }
-        
     })
 
 
+function traziFilm() {
+    wrapper.innerHTML = '';
+    const fraza = document.getElementById('iPretraga2').value.toLowerCase();
 
-    function traziFilm() { 
-        let input = document.getElementById('iPretraga2').value 
-        input=input.toLowerCase(); 
-        let film = document.getElementsByClassName('filmovi'); 
-          
-        for (i = 0; i < film.length; i++) {  
-            if (!film[i].innerHTML.toLowerCase().includes(input)) { 
-                film[i].style.display="none"; 
-            } 
-            else { 
-                film[i].style.display="list-item";                  
-            } 
-        } 
-    } 
-    
+    for (let i = 0; i < filmovi.length; i++) {
+        if (fraza.length >= 1 && filmovi[i].naziv.toLowerCase().includes(fraza)) {
+            wrapper.innerHTML += `
+            <br> Naziv:  ${filmovi[i].naziv} <br>
+            Godina: ${filmovi[i].godina} <br>
+            Slika: <img src='${filmovi[i].slika}' height="200px", width="150px"> <hr>
+            `;
+        }
+    }
+}
+
